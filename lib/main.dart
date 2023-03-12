893f88eba6633f2c2544ab82de52d446aebd67db
G M Saiful Alam <59245133+SaifulAlam93@users.noreply.github.com>
2023-03-13 01:28:45 +0600

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
+import 'package:my_app/page1.dart';
+import 'package:my_app/page2.dart';
+import 'package:my_app/page3.dart';
 
 void main() {
-  runApp(MyWidget33());
-}
-
-class MyWidget33 extends StatelessWidget {
-  // const MyWidget33({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.blue),
-      darkTheme: ThemeData(primarySwatch: Colors.cyan),
-      debugShowCheckedModeBanner: false,
-      home: Practice3(),
-    );
-  }
-}
-
-MySnackBar(String message, BuildContext context) {
-  return ScaffoldMessenger.of(context)
-      .showSnackBar(SnackBar(content: Text(message)));
-}
-
-class Prac2 extends StatelessWidget {
-  const Prac2({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("My APP"),
-        // titleSpacing: 0,
-        centerTitle: true,
-        toolbarHeight: 60, //Defoult height 60
-        toolbarOpacity: 1,
-        elevation: 5,
-        backgroundColor: Color.fromARGB(255, 71, 112, 107),
-        actions: [
-          IconButton(
-              onPressed: () {
-                print("account_balance Clicked----");
-              },
-              icon: Icon(Icons.account_balance)),
-          IconButton(
-              onPressed: () {
-                print("2ND Clicked----");
-              },
-              icon: Icon(Icons.accessible_forward_sharp))
-        ],
-      ),
-      body: Container(
-        child: Text("Hello World!"),
-      ),
-      drawer: Container(
-        child: Container(
-          child: Text("Hello World!"),
-          color: Colors.blueAccent,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          MySnackBar("Hi!! ", context);
-          // print("FloatingActionButton");
-        },
-        // elevation: 10,
-        child: Icon(Icons.add_call),
-        // child: Text("Add"),
-        backgroundColor: Color.fromARGB(255, 61, 110, 86),
-      ),
-    );
-  }
+  runApp(MyFirstStateFullW());
 }
 
 class MyApp extends StatelessWidget {
@@ -81,7 +17,467 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.blue),
       darkTheme: ThemeData(primarySwatch: Colors.cyan),
       debugShowCheckedModeBanner: false,
-      home: MyWidget(),
+      home: PracticeN11(),
+    );
+  }
+}
+
+class MyFirstStateFullW extends StatefulWidget {
+  const MyFirstStateFullW({Key? key}) : super(key: key);
+
+  @override
+  State<MyFirstStateFullW> createState() => _MyFirstStateFullWState();
+}
+
+class _MyFirstStateFullWState extends State<MyFirstStateFullW> {
+  PageController _controller = PageController(initialPage: 0);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: PageView(
+        controller: _controller,
+        children: [
+          Page1(),
+          Page2(),
+          Page3(),
+        ],
+      )),
+    );
+  }
+}
+
+class PracticeN11 extends StatelessWidget {
+  const PracticeN11({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
+
+//Grid View
+class PcticeN10 extends StatelessWidget {
+  const PcticeN10({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GridView.count(
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
+              crossAxisCount: 2,
+              children: [
+                Container(
+                  color: Colors.amberAccent,
+                  height: 250,
+                  width: 250,
+                ),
+                Container(
+                  color: Colors.cyan,
+                  height: 250,
+                  width: 250,
+                ),
+                Container(
+                  color: Colors.teal,
+                  height: 250,
+                  width: 250,
+                ),
+                Container(
+                  color: Colors.amberAccent,
+                  height: 250,
+                  width: 250,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//Card View
+class PracticeN9 extends StatefulWidget {
+  const PracticeN9({super.key});
+
+  @override
+  State<PracticeN9> createState() => _PracticeN9State();
+}
+
+class _PracticeN9State extends State<PracticeN9> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+            body: Center(
+          child: Card(
+            elevation: 50,
+            shape: RoundedRectangleBorder(
+              // borderRadius: BorderRadius.all(
+              //   Radius.circular(20),
+              // ),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+              ),
+            ),
+          ),
+        )),
+      ),
+    );
+  }
+}
+
+// LiquidSwipe--- Page change style
+class PracticeN8 extends StatelessWidget {
+  const PracticeN8({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final pages = [
+      Container(
+        color: Colors.amberAccent,
+        height: 250,
+        width: 250,
+      ),
+      Container(
+        color: Colors.cyan,
+        height: 250,
+        width: 250,
+      ),
+      Container(
+        color: Colors.teal,
+        height: 250,
+        width: 250,
+      ),
+      Container(
+        color: Colors.amberAccent,
+        height: 250,
+        width: 250,
+      ),
+      Container(
+        color: Colors.cyan,
+        height: 250,
+        width: 250,
+      ),
+      Container(
+        color: Colors.teal,
+        height: 250,
+        width: 250,
+      ),
+    ];
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: LiquidSwipe(pages: pages),
+        ),
+      ),
+    );
+  }
+}
+
+//Show Image
+
+class PracticeN7 extends StatelessWidget {
+  const PracticeN7({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(body: Center(child: Image.asset("images/test.jpg"))),
+      ),
+    );
+  }
+}
+
+//Stack  It used to create weget up above another weget.
+class PracticeN6 extends StatelessWidget {
+  const PracticeN6({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+            body: Center(
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height / 5,
+                width: MediaQuery.of(context).size.width / 3,
+                color: Colors.yellow,
+              ),
+              Positioned(
+                  bottom: -50,
+                  child: CircleAvatar(
+                    radius: 30,
+                  ))
+            ],
+          ),
+        )),
+      ),
+    );
+  }
+}
+
+//ListTail It used to design a list of data
+class PracticeN5 extends StatelessWidget {
+  const PracticeN5({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: ListView(
+            children: [
+              ListTile(
+                title: Text("List Tile 1"),
+                subtitle: Text("ListTile practice with saiful.."),
+                leading: CircleAvatar(
+                  child: Icon(Icons.add_call),
+                ),
+                trailing: Icon(Icons.account_circle),
+              ),
+              ListTile(
+                title: Text("List Tile 1"),
+                subtitle: Text("ListTile practice with saiful.."),
+                leading: CircleAvatar(
+                  child: Icon(Icons.add_call),
+                ),
+                trailing: Icon(Icons.account_circle),
+              ),
+              ListTile(
+                title: Text("List Tile 1"),
+                subtitle: Text("ListTile practice with saiful.."),
+                leading: CircleAvatar(
+                  child: Icon(Icons.add_call),
+                ),
+                trailing: Icon(Icons.account_circle),
+              ),
+              ListTile(
+                title: Text("List Tile 1"),
+                subtitle: Text("ListTile practice with saiful.."),
+                leading: CircleAvatar(
+                  child: Icon(Icons.add_call),
+                ),
+                trailing: Icon(Icons.account_circle),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// MediaQuery use to make responsive UI
+class PracticeN4 extends StatelessWidget {
+  const PracticeN4({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: Container(
+              height: MediaQuery.of(context).size.height / 5,
+              width: MediaQuery.of(context).size.width / 3,
+              color: Colors.yellow,
+              child: const Center(
+                child: Text(
+                  "Hello",
+                  style: TextStyle(
+                      color: Colors.cyan,
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//List View
+class PracticeN3 extends StatelessWidget {
+  const PracticeN3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView(children: [
+          Container(
+            color: Colors.amberAccent,
+            height: 250,
+            width: 250,
+          ),
+          Container(
+            color: Colors.cyan,
+            height: 250,
+            width: 250,
+          ),
+          Container(
+            color: Colors.teal,
+            height: 250,
+            width: 250,
+          ),
+          Container(
+            color: Colors.amberAccent,
+            height: 250,
+            width: 250,
+          ),
+          Container(
+            color: Colors.cyan,
+            height: 250,
+            width: 250,
+          ),
+          Container(
+            color: Colors.teal,
+            height: 250,
+            width: 250,
+          ),
+          Container(
+            color: Colors.amberAccent,
+            height: 250,
+            width: 250,
+          ),
+          Container(
+            color: Colors.cyan,
+            height: 250,
+            width: 250,
+          ),
+          Container(
+            color: Colors.teal,
+            height: 250,
+            width: 250,
+          ),
+        ]),
+      ),
+    );
+  }
+}
+
+//Row
+class PracticeN2 extends StatelessWidget {
+  const PracticeN2({Key? key}) : super(key: key);
+
+  // get controller => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(children: [
+            Container(
+              color: Colors.amberAccent,
+              height: double.infinity,
+              width: 100,
+            ),
+            Container(
+              color: Colors.cyan,
+              width: 100.0,
+              height: double.infinity,
+            ),
+            Container(
+              color: Colors.teal,
+              width: 100.0,
+              height: double.infinity,
+            ),
+            Container(
+              color: Colors.amberAccent,
+              width: 100.0,
+              height: double.infinity,
+            ),
+            Container(
+              color: Colors.cyan,
+              width: 100.0,
+              height: double.infinity,
+            ),
+            Container(
+              color: Colors.teal,
+              width: 100.0,
+              height: double.infinity,
+            ),
+            Container(
+              color: Colors.amberAccent,
+              width: 100.0,
+              height: double.infinity,
+            ),
+            Container(
+              color: Colors.cyan,
+              width: 100.0,
+              height: double.infinity,
+            ),
+            Container(
+              color: Colors.teal,
+              width: 100.0,
+              height: double.infinity,
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+//Safe Area
+class PracticeN1 extends StatelessWidget {
+  const PracticeN1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          body: Container(
+            height: double.infinity,
+            width: 200.0,
+            color: Colors.yellow,
+            child: const Center(
+              child: Text(
+                "Hello",
+                style: TextStyle(
+                    color: Colors.cyan,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
@@ -251,26 +647,24 @@ class Practice3 extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 500.0,
-              width: double.infinity,
-              color: Colors.blueAccent,
-            ),
-            Container(
-              height: 500.0,
-              width: double.infinity,
-              color: Colors.redAccent,
-            ),
-            Container(
-              height: 500.0,
-              width: double.infinity,
-              color: Colors.green,
-            ),
-          ],
-        ),
+          body: Column(
+        children: [
+          Container(
+            height: 200.0,
+            width: double.infinity,
+            color: Colors.blueAccent,
+          ),
+          Container(
+            height: 200.0,
+            width: double.infinity,
+            color: Colors.redAccent,
+          ),
+          Container(
+            height: 200.0,
+            width: double.infinity,
+            color: Colors.green,
+          ),
+        ],
       )),
     );
   }
@@ -288,16 +682,16 @@ class Practice4 extends StatelessWidget {
         children: [
           Container(
             height: double.infinity,
-            width: 500.0,
+            width: 200.0,
             color: Colors.blueAccent,
           ),
           Container(
-            width: 500.0,
+            width: 200.0,
             height: double.infinity,
             color: Colors.redAccent,
           ),
           Container(
-            width: 500.0,
+            width: 200.0,
             height: double.infinity,
             color: Colors.green,
           ),
@@ -376,7 +770,7 @@ class Practice5 extends StatelessWidget {
   }
 }
 
-//Dynamic hight (Expanded)
+//Dynamic hight (Expanded) without scrol view..
 class Practice6 extends StatelessWidget {
   const Practice6({Key? key}) : super(key: key);
 
@@ -407,6 +801,27 @@ class Practice6 extends StatelessWidget {
               height: 200.0,
               width: double.infinity,
               color: Colors.green,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              height: 200.0,
+              width: double.infinity,
+              color: Colors.redAccent,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              height: 200.0,
+              width: double.infinity,
+              color: Colors.green,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              height: 200.0,
+              width: double.infinity,
+              color: Colors.redAccent,
             ),
           ),
         ],
